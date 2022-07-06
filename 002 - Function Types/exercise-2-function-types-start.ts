cd9f3e107170401592780b5cea90373801495198
Manfredi <manfrediorlando@yahoo.it>
2022-07-06 22:27:22 +0200

    es 1-10

--- 002 - Function Types/exercise-2-function-types-start.ts
+++ 002 - Function Types/exercise-2-function-types-start.ts
@@ -12,17 +12,17 @@ function doubleThePopulation(value) {
 
 doubleThePopulation(5);
 
-doubleThePopulation("6.12");
+doubleThePopulation(6.12);
 
 doubleThePopulation(8_526);
 
-doubleThePopulation(false);
+
 
 // Alter this function so the `language2` parameter is optional.
 // Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
 
-function languagesSpoken(country: string, language1: string, language2: string): void {
-    console.log(`The languages spoken in ${country} are:`);
+function languagesSpoken(country: string, language1: string, language2?: string): void {
+    console.log(`The languages spoken in ${country} are: ${language1}, ${language2}`);
 
     console.log(language1);
 
